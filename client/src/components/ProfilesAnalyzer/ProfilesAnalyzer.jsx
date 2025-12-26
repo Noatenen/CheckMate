df1fe238d5047ddf1bbb17aa202c15a392db5e4f
Maysibony <sibonymay@gmail.com>
2025-12-26 14:19:54 +0200

    Apdate Css of Profiles page

--- client/src/components/ProfilesAnalyzer/ProfilesAnalyzer.jsx
+++ client/src/components/ProfilesAnalyzer/ProfilesAnalyzer.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import styles from './ProfilesAnalyzer.module.css';
+
 
 function ProfilesAnalyzer() {
   const [loadingText, setLoadingText] = useState('');
@@ -12,6 +14,12 @@ function ProfilesAnalyzer() {
       <h2>Social Profiles Safety Analyzer</h2>
       <textarea placeholder="Paste username here..." />
       <br />
+      <h3>Which platform?</h3>
+      <button className={styles.Platformbutton}>Instagram</button>
+      <button className={styles.Platformbutton}>Facebook</button>
+      <button className={styles.Platformbutton}>Phone Number</button>
+      <button className={styles.Platformbutton}>TikTok</button>
+      <br /><br /><br />
       <button onClick={handleAnalyze}>Analyze</button>
       <h6>{loadingText}</h6>
     </div>
